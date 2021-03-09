9628b590d3d2a7641c04d835f87df609a2a6e26a
Joan Manrubia <jmanrumartinez@gmail.com>
2021-03-09 20:41:34 +0100

    render some notifications to test it

--- src/index.js
+++ src/index.js
@@ -1,6 +1,29 @@
 import React from 'react'
-import styles from './styles.module.css'
+import Notification, { notificationTypes } from './Notification'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+export const ExampleComponent = () => {
+  return (
+    <div>
+      <Notification
+        title='Error'
+        message='An error has ocurred'
+        type={notificationTypes.error}
+      />
+      <Notification
+        title='Info'
+        message='Something was updated'
+        type={notificationTypes.info}
+      />
+      <Notification
+        title='Warning'
+        message='Something went wrong'
+        type={notificationTypes.warning}
+      />
+      <Notification
+        title='Success'
+        message='Something went well'
+        type={notificationTypes.success}
+      />
+    </div>
+  )
 }
