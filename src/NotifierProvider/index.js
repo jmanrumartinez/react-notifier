35b35f124d102ac1c03c176f8745360045c8f965
Joan Manrubia <jmanrumartinez@gmail.com>
2021-03-13 17:59:09 +0100

    created notifierReducer & hooks for behaviour

--- src/NotifierProvider/index.js
+++ src/NotifierProvider/index.js
@@ -1,10 +1,54 @@
-import React, { createContext } from 'react'
+import React, { createContext, useContext, useReducer } from 'react'
 import PropTypes from 'prop-types'
+import Notification from '../Notification'
+import { generateUuid } from '../utils'
+
+const NOTIFIER_TYPES = {
+  ADD: 'ADD',
+  REMOVE: 'REMOVE'
+}
+
+const notifierReducer = (state, action) => {
+  const { type: actionType, payload } = action
+
+  switch (actionType) {
+    case NOTIFIER_TYPES.ADD:
+      return [
+        ...state,
+        {
+          id: generateUuid(),
+          type: payload.type,
+          title: payload.title,
+          message: payload.message
+        }
+      ]
+
+    case NOTIFIER_TYPES.REMOVE:
+      return state.filter((notification) => notification.id !== payload.id)
+    default:
+      throw new Error('Action type not supported for notifierReducer')
+  }
+}
 
 const NotifierContext = createContext()
 
 const NotifierProvider = ({ children }) => {
-  return <NotifierContext.Provider>{children}</NotifierContext.Provider>
+  const [notifications, dispatch] = useReducer(notifierReducer, [])
+
+  return (
+    <NotifierContext.Provider value={{ dispatch }}>
+      {children}
+      {notifications.map((notification) => (
+        <Notification
+          key={notification.id}
+          id={notification.id}
+          title={notification.title}
+          message={notification.message}
+          type={notification.type}
+        />
+      ))}
+    </NotifierContext.Provider>
+  )
 }
 
 NotifierProvider.propTypes = {
@@ -12,3 +56,36 @@ NotifierProvider.propTypes = {
 }
 
 export default NotifierProvider
+
+const useNotifierContext = () => {
+  const context = useContext(NotifierContext)
+  if (!context)
+    throw new Error('useNotify should be used within a NotifierProvider')
+
+  return context
+}
+
+// TODO: A function to add Notification to the queue to be displayed
+export const useNotify = () => {
+  const context = useNotifierContext()
+
+  const notify = ({ type, title, message }) =>
+    context.dispatch({
+      type: NOTIFIER_TYPES.ADD,
+      payload: { type, title, message }
+    })
+
+  return { notify }
+}
+
+export const useNotifyRemove = () => {
+  const context = useNotifierContext()
+
+  const remove = ({ id }) =>
+    context.dispatch({
+      type: NOTIFIER_TYPES.REMOVE,
+      payload: { id }
+    })
+
+  return { remove }
+}
--- src/useNotify.js
+++
@@ -1,4 +0,0 @@
-// TODO: A function to add Notification to the queue to be displayed
-const useNotify = () => {}
-
-export default useNotify
