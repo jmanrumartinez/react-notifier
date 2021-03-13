b3b9572842fb4ce7c593f9bab33b0c04cabc31bc
Joan Manrubia <jmanrumartinez@gmail.com>
2021-03-13 17:56:15 +0100

    create uuid generator util fn

--- src/utils.js
+++ src/utils.js
@@ -1,2 +1,10 @@
 export const capitalize = (string) =>
   string.charAt(0).toUpperCase() + string.slice(1)
+
+export const generateUuid = () => {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+    var r = (Math.random() * 16) | 0
+    var v = c === 'x' ? r : (r & 0x3) | 0x8
+    return v.toString(16)
+  })
+}
